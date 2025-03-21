d6a066a310c0b3cacad11635bd0d48ca9ed2c4d0
Juan pablo <juan.manzo99@uabc.edu.mx>
2025-03-20 21:27:58 -0700

    actualizacion para peralta,falta subir

--- mi_app/views.py
+++ mi_app/views.py
@@ -12,9 +12,44 @@ from django.shortcuts import render
 # mi_app/views.py
 
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+# Ruta de los archivos
+ADMIN_FILE = "mi_app/static/TXT/ADMIN.txt"
+USER_FILE = "mi_app/static/TXT/USER.txt"
+
 
 def login_view(request):
-    return render(request, 'mi_app/login.html')  # Asegúrate de que la plantilla esté en 'mi_app/login.html'
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        # Verificar si es admin
+        if validar_usuario(username, password, ADMIN_FILE):
+            return redirect("HOME_ADMIN")  # Redirige a la página de admin
+
+        # Verificar si es usuario normal
+        elif validar_usuario(username, password, USER_FILE):
+            return redirect("HOME_USER")  # Redirige a la página de usuario
+
+        else:
+            messages.error(request, "Usuario o contraseña incorrectos")
+
+    return render(request, "mi_app/login.html")
+
+
+def validar_usuario(username, password, file_path):
+    try:
+        with open(file_path, "r") as file:
+            for line in file:
+                user, passw = line.strip().split(",")
+                if user == username and passw == password:
+                    return True
+    except FileNotFoundError:
+        print(f"El archivo {file_path} no existe")
+    return False
+
 
 def RECOVERY(request):
     return render(request, 'mi_app/Recovery.html')
